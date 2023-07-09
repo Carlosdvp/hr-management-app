ff7ccbeb6b56af61f3870fd8312b428825c237e1
Carlosdvp <carlosdvp.tx@gmail.com>
2023-07-08 20:05:59 -0400

    completed the basic crdu operations, added routes and controllers for each.

--- server/src/controllers/user.controllers.ts
+++ server/src/controllers/user.controllers.ts
@@ -7,4 +7,54 @@ export const userController = {
 
     return res.json(users);
   },
+  async addUser(req: Request, res: Response) {
+    const userData = req.body;
+    const user = await prisma.user.create({
+      data: {
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        email: userData.email,
+        password: userData.password,
+      }
+    });
+
+    return res.json({ user: user });
+  },
+  async findUniqueUser(req: Request, res: Response) {
+    const paramEmail = req.params.email;
+
+    const uniqueUser = await prisma.user.findUnique({
+      where: {
+        email: paramEmail,
+      }
+    });
+
+    return res.json({ uniqueUser: uniqueUser });
+  },
+  async updateUser(req: Request, res: Response) {
+    const paramEmail = req.params.email;
+    const userName = req.body.firstName;
+
+    const updatedUser = await prisma.user.update({
+      data: {
+        firstName: userName,
+      },
+      where: {
+        email: paramEmail,
+      },
+    });
+
+    return res.json({ updatedUser: updatedUser });
+  },
+  async deleteUser(req: Request, res: Response) {
+    const paramEmail = req.params.email;
+
+    const deletedUser = await prisma.user.delete({
+      where: {
+        email: paramEmail,
+      },
+    });
+
+    return res.json({ deletedUser: deletedUser });
+  },
 };
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -4,5 +4,9 @@ import { userController } from "../controllers/user.controllers";
 const routes = Router();
 
 routes.get("/", userController.getUsers);
+routes.post("/add-user", userController.addUser);
+routes.get("/:email", userController.findUniqueUser);
+routes.put('/:email', userController.updateUser);
+routes.delete('/:email', userController.deleteUser);
 
 export default routes;
