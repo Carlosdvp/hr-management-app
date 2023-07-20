514ccc4fbfe60e6becc33b901302e3d2b011ee84
Carlosdvp <carlosdvp.tx@gmail.com>
2023-07-20 18:35:00 -0400

    made good progress with the profile page markup and styling.

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -9,11 +9,9 @@ const toggleSideBar = (): void => {
 </script>
 
 <template>
-    <!-- Hambuger menu -->
   <div 
     class="cursor-pointer w-[40px] h-[50px] bg-slate-800 py-4 pl-2"
-    @click="toggleSideBar"
-  >
+    @click="toggleSideBar">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-[25px] h-[25px]">
       <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" fill="white" />
     </svg>
@@ -26,13 +24,17 @@ const toggleSideBar = (): void => {
     <div class="h-[calc(100vh-50px)] bg-slate-600 border-x-2 border-x-slate-400">
       <div class="flex flex-col justify-between items-center h-full">
         <div class="flex flex-col">
-          <router-link to="/" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800  transition duration-400 ease-in-out">
+          <router-link
+            to="/"
+            class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800  transition duration-400 ease-in-out">
             <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path>
             </svg>
             Home
           </router-link>
-          <router-link to="/profile-page" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300  hover:text-gray-800 transition duration-400 ease-in-out">
+          <router-link
+            to="/profile-page"
+            class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300  hover:text-gray-800 transition duration-400 ease-in-out">
             <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M2 9.5A3.5 3.5 0 005.5 13H9v2.586l-1.293-1.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 15.586V13h2.5a4.5 4.5 0 10-.616-8.958 4.002 4.002 0 10-7.753 1.977A3.5 3.5 0 002 9.5zm9 3.5H9V8a1 1 0 012 0v5z" clip-rule="evenodd"></path>
             </svg>
@@ -41,7 +43,9 @@ const toggleSideBar = (): void => {
         </div>
         <div class="h-[50px]">
           <div>
-            <router-link to="/" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300 hover:text-gray-800  transition duration-400 ease-in-out">
+            <router-link
+              to="/"
+              class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300 hover:text-gray-800  transition duration-400 ease-in-out">
               <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path>
               </svg>
--- frontend/src/views/ProfilePage.vue
+++ frontend/src/views/ProfilePage.vue
@@ -1,11 +1,127 @@
 <script setup lang="ts">
 import Navbar from '@/components/Navbar.vue';
-
 </script>
 
 <template>
   <Navbar />
-  <div class="w-[100vw] p-7 bg-slate-300 h-[100vh]">
-    profile Page
+
+  <div 
+    class="w-[100vw] py-2 bg-slate-300 h-[100vh] flex flex-col items-center">
+    
+    <header class="flex bg-yellow-400 w-full border border-y-red-500">
+      <!-- img icon  for the user showing a pisture or their initials -->
+      <img src="" alt="icon" class="bg-blue-400 w-10 m-2">
+      <h1 class="p-3 font-semibold text-lg">username</h1>
+    </header>
+
+    <main class="grid grid-cols-1 gap-2 pt-4 w-[90%] max-w-6xl">
+      <div class="grid grid-cols-2 gap-4 bg-white p-4">
+        <!-- First column -->
+        <div class="px-4">
+          <div class="mb-4 flex justify-between">
+            <label class="mr-2 text-gray-600">User ID:</label>
+            <input type="text" value="12345" class="w-[70%] bg-gray-100 mx-2 p-1 text-slate-400 rounded" readonly />
+          </div>
+
+          <div class="mb-4 flex justify-between">
+            <label class="block text-gray-600">First Name:</label>
+            <input type="text" class="w-[70%] border border-gray-300 mx-2 py-1 rounded" />
+          </div>
+
+          <div class="mb-4 flex justify-between">
+            <label class="block text-gray-600">Last Name:</label>
+            <input type="text" class="w-[70%] border border-gray-300 mx-2 py-1 rounded" />
+          </div>
+        </div>
+
+        <!-- Second column -->
+        <div class="px-4">
+          <div class="mb-4 flex justify-between">
+            <label class="block text-gray-600">Email:</label>
+            <input type="email" class="w-[80%] border border-gray-300 mx-2 py-1 rounded" />
+          </div>
+
+          <div class="mb-4 flex justify-between">
+            <label class="block text-gray-600">Mobile:</label>
+            <input type="tel" class="w-[80%] border border-gray-300 mx-2 py-1 rounded" />
+          </div>
+
+          <div class="mb-4 flex justify-between">
+            <label class="block text-gray-600">Country:</label>
+            <select class="w-[80%] border border-gray-300 mx-2 p-1 text-slate-500 rounded">
+              <option value="">Select a Country</option>
+              <!-- Add your list of countries here as <option> elements -->
+            </select>
+          </div>
+
+          <div class="mb-4 flex justify-between">
+            <label class="block text-gray-600">City:</label>
+            <input type="text" class="w-[80%] border border-gray-300 mx-2 py-1 rounded" />
+          </div>
+        </div>
+      </div>
+
+      <div class="grid grid-cols-2 gap-4 bg-white p-4">
+        <!-- First column -->
+        <div class="w-[80%] mx-auto my-0">
+          <div class="mb-4 flex justify-between">
+            <label class="mr-2 text-gray-600">Developer Type:</label>
+            <select class="w-[60%] border border-gray-300 px-2 p-1 text-slate-500 rounded">
+              <option value="">Select a developer type</option>
+              <!-- Add your list of options here as <option> elements -->
+            </select>
+          </div>
+
+          <div class="mb-4 flex justify-between">
+            <label class="block text-gray-600">Main Tech Stack:</label>
+            <textarea class="w-[70%] h-32 border border-gray-300 px-2 p-1 text-slate-500 rounded"></textarea>
+          </div>
+        </div>
+
+        <!-- Second column -->
+        <div class="w-[80%] mx-auto my-0">
+          <div class="mb-4 flex justify-between">
+            <label class="mr-2 text-gray-600">Years of Experience:</label>
+            <select class="w-[60%] border border-gray-300 px-2 p-1 text-slate-500 rounded">
+              <option value="">Select years of experience</option>
+              <option value="1">1 year</option>
+              <option value="2">2 years</option>
+              <option value="3">3 years</option>
+              <option value="4">4 years</option>
+              <option value="5">5 years</option>
+              <option value="6">6 years</option>
+              <option value="7">7 years</option>
+              <option value="8">8 years</option>
+            </select>
+          </div>
+
+          <div class="mb-4 flex justify-between">
+            <label class="mr-2 text-gray-600">English Level:</label>
+            <select class="w-[60%] border border-gray-300 px-2 p-1 text-slate-500 rounded">
+              <option value="">Select English level</option>
+              <option value="basic">Basic</option>
+              <option value="intermediate">Intermediate</option>
+              <option value="advanced">Advanced</option>
+              <option value="fluent">Fluent</option>
+            </select>
+          </div>
+
+          <div class="flex items-center">
+            <!-- Empty space (placeholder) -->
+          </div>
+
+          <div class="flex items-center">
+            <!-- Empty space (placeholder) -->
+          </div>
+        </div>
+      </div>
+
+      <div class="grid grid-rows-3 bg-white p-4">
+
+      </div>
+      <div class="grid grid-rows-2 bg-white p-4">
+
+      </div>
+    </main>
   </div>
 </template>
